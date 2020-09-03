d06d241ebf71a2573374c09b3a975f6f3884eaf3
Clint Lauby <thecmachine05@gmail.com>
2020-09-03 10:24:00 -0400

    species and name lengths enforced

--- rhinoceros.js
+++ rhinoceros.js
@@ -1,5 +1,6 @@
 const uuidv4 = require('uuid/v4');
 let rhinoceroses = require('./data');
+let species = require('./speciesData');
 
 exports.getAll = () => {
   return rhinoceroses;
@@ -12,13 +13,17 @@ exports.getOne = data => {
 };
 
 exports.newRhinoceros = data => {
-  const newRhino = {
-    id: uuidv4(),
-    // name: 'clint',
-    // species: 'unicorn',
-    name: data.name,
-    species: data.species,
+  const nameLength = data.name.length;
+  if(nameLength > 0 && nameLength < 21 && species.includes(data.species)){
+    const newRhino = {
+      id: uuidv4(),
+        name: data.name,
+        species: data.species,
+    }
+    rhinoceroses.push(newRhino);
+  }else{
+    //return error with only certain keys
+    return "Error: Name must be 1-20 characters with valid species"
   };
-  rhinoceroses.push(newRhino);
   return data;
 };
---
+++ speciesData.js
@@ -0,0 +1,7 @@
+module.exports = [
+  'white_rhinoceros', 
+  'black_rhinoceros', 
+  'indian_rhinoceros', 
+  'javan_rhinoceros',
+  'sumatran_rhinoceros'
+];
